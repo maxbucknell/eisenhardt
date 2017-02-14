89ca6d4aa2e0a113cfcdd6ecb332a07842892c18
Max Bucknell <me@maxbucknell.com>
2017-02-14 15:37:05 +0000

    Update Info command with table.

--- src/Redbox/RD/Command/InfoCommand.php
+++ src/Redbox/RD/Command/InfoCommand.php
@@ -11,6 +11,7 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Helper\Table;
 use Redbox\RD\Project;
 use Redbox\RD\ProjectFactory;
 
@@ -51,7 +52,7 @@ EOT
         $workingDirectory = $this->project->getInstallationDirectory();
         chdir($workingDirectory);
 
-        passthru(<<<CMD
+        $result = shell_exec(<<<CMD
 docker-compose \
   -p rd \
   -f .rd/base.yml \
@@ -62,5 +63,48 @@ docker-compose \
   ps
 CMD
         );
+
+        $rows = explode("\n", $result);
+        $containers = array_slice($rows, 2, -1);
+        $rows = array_map(
+            function ($container) {
+                $row = [];
+                $isUp = strpos($container, 'Up') !== false;
+
+                $row['name'] = explode(" ", $container)[0];
+                $row['status'] = $isUp ? '<bg=green;fg=black> UP </>' : '<bg=red;fg=white;options=bold>DOWN</>';
+                $row['ip'] = $isUp ? $this->getIPAddress($row['name']) : '<not running>';
+
+                return $row;
+            },
+            $containers
+        );
+
+        $infoTable = new Table($output);
+        $infoTable
+            ->setHeaders(['Container', 'Status', 'IP Address'])
+            ->setRows(array_map(
+                function ($row) {
+                    return [
+                        $row['name'],
+                        $row['status'],
+                        $row['ip']
+                    ];
+                },
+                $rows
+            ))
+            ->render();
+    }
+
+    private function getIPAddress($containerName)
+    {
+        $infoJson = shell_exec(<<<CMD
+docker inspect {$containerName}
+CMD
+        );
+
+        $info = json_decode($infoJson, true);
+
+        return $info[0]['NetworkSettings']['Networks']['rd_magento']['IPAddress'];
     }
 }
